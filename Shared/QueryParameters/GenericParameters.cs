3067e9c78eaad532523f3eddf40d2e6bd9a2a9ea
Darlin Ruben Nina C. <78616515+Ruben890@users.noreply.github.com>
2025-01-15 15:19:51 -0400

    .

--- Shared/QueryParameters/GenericParameters.cs
+++ Shared/QueryParameters/GenericParameters.cs
@@ -1,4 +1,5 @@
 ﻿using HealthCenterAPI.Shared.RequestFeatures;
+using System.Diagnostics.CodeAnalysis;
 
 namespace HealthCenterAPI.Shared.QueryParameters
 {
@@ -9,7 +10,15 @@ namespace HealthCenterAPI.Shared.QueryParameters
         public string? Sector {  get; set; } = null;
         public string? Level { get; set; } = null!;
         public string? TypeCenter { get; set; } = null!;
-
         public string Area { get; set; } = null!;
+        public bool? isOffices { get; set; } = null!;
+        public bool? isDentistry { get; set; } = null!;
+        public bool? isEmergency { get; set; } = null!;
+        public bool? isLaboratory { get; set; } = null!;
+        public bool? isSonography { get; set; } = null!;
+        public bool? isPhysiotherapy { get; set; } = null!;
+        public bool? isInternet { get; set; } = null!;
+        public bool? Xray { get; set; } = null!;
+
     }
 }
